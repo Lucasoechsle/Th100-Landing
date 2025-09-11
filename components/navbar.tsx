af6ebbae884ffc550578b6c52c962e44872da171
joacojachow <joacojachow@hotmail.com>
2025-09-11 12:29:17 -0300

    navbar centred

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,9 +16,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="flex-shrink-0 flex items-center">
-              <span className="text-2xl font-bold text-white">TH100</span>
-            </Link>
+            <span className="text-2xl font-bold text-white">TH100</span>
           </div>
           <div className="hidden md:flex items-center space-x-8">
             <Link href="/" className="text-white hover:text-gray-200 font-medium">
